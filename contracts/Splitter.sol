0d428a4e7612812aa944f033b18345a101c3b631
ArnaudovSt <stefan.arnaudov@consensys.net>
2019-04-24 17:05:21 +0100

    added Split event.

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -4,6 +4,8 @@ pragma solidity 0.5.7;
 contract Splitter {
 	uint16 constant RECIPIENTS_MAX_LEN = 1024;
 
+	event Split(address indexed from, uint256 share);
+
 	modifier lengthRestricted(uint256 _len) {
 		require(_len <= RECIPIENTS_MAX_LEN, "Passed len greater than recipients max len.");
 		_;
@@ -16,6 +18,8 @@ contract Splitter {
 	function split(address payable[] calldata recipients) external payable lengthRestricted(recipients.length) {
 		uint256 tip = msg.value % recipients.length;
 		uint256 share = msg.value / recipients.length;
+
+		emit Split(msg.sender, share);
 
 		for (uint16 i = 0; i < recipients.length; i++) {
 			address payable recipient = recipients[i];
